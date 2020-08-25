72e075e71b950b6e40a88097c9b10ecdb71ac992
PersianBuddy <mnajafi0014@gmail.com>
2020-08-25 18:57:57 +0430

    Add TODO list and .gitignore

---
+++ main.py
@@ -0,0 +1,6 @@
+# TODO: create a classified folder to copy files (or move) in it
+# TODO: search for all files and directories in current dierctory
+# TODO: loop throgh them and diretories in anoter list
+# TODO: if it's a file get it's extention and create a folder in classified folder for it and add extention to extention list
+# TODO: for all files check if extention exist in extention list then copy (move) it to "classifed folder/extention folder"
+# TODO: when all files in this directory had been checked then remove diretory form directory list
