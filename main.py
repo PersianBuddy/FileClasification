7b520a2260c334f783f8fddbd06dab931e307e1f
PersianBuddy <mnajafi0014@gmail.com>
2020-08-25 20:25:54 +0430

    Write main program

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-# TODO: create a classified folder to copy files (or move) in it
-# TODO: search for all files and directories in current dierctory
-# TODO: loop throgh them and diretories in anoter list
-# TODO: if it's a file get it's extention and create a folder in classified folder for it and add extention to extention list
-# TODO: for all files check if extention exist in extention list then copy (move) it to "classifed folder/extention folder"
-# TODO: when all files in this directory had been checked then remove diretory form directory list
+import os
+import shutil
+
+classified_directory = os.path.join(os.path.curdir, 'classified')
+if not os.path.exists(classified_directory):
+    os.mkdir(classified_directory)
+direcotories_list = [os.path.curdir]
+
+while direcotories_list:
+    current_directory = direcotories_list.pop()
+    # search for all files and directories in current dierctory
+    direcotory_content = os.listdir(current_directory)
+    for directory_name in direcotory_content:
+        if directory_name == '.git' or directory_name == 'classified':
+            continue
+        elif os.path.isdir(os.path.join(current_directory, directory_name)):
+            direcotories_list.append(os.path.join(current_directory, directory_name))
+
+        # if it's a file classify it
+        else:
+            file_path = os.path.join(current_directory, directory_name)
+            file_extention = os.path.splitext(file_path)[1]
+            # file with no extention
+            if file_extention =='':
+                other_extention_path = os.path.join(classified_directory, 'other')
+                if not os.path.exists(other_extention_path):
+                    os.mkdir(other_extention_path)
+                # TODO: check if file exist
+                shutil.copy(file_path, other_extention_path)
+            # files with extentions
+            else:
+                with_extention_files_path = os.path.join(classified_directory, file_extention)
+                if not os.path.exists(with_extention_files_path):
+                    os.mkdir(with_extention_files_path)
+                # TODO: check if file exist
+                shutil.copy(file_path, with_extention_files_path)
